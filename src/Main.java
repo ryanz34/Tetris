b7bdc257570a2b024f7708de5b3d7172758b4093
Ryan <ryanzhang0112@gmail.com>
2018-01-20 22:32:30 -0500

    Block Images

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.MouseInfo;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.rmi.server.ExportException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -37,7 +41,6 @@ public class Main extends JFrame implements ActionListener {
             game.move();
             game.repaint();
         }
-
 
     }
 
@@ -119,36 +122,57 @@ class GamePanel extends JPanel implements KeyListener {
         add(O_STATES);
     }};
 
+    private HashMap<Integer, BufferedImage> blocks = new HashMap<Integer, BufferedImage>();
+
     private int stateNum;
 
     private boolean placed;
 
-    int x, y;
+    private int x, y;
     private Integer[][] board;
 
-    int score = 0;
+    private int score = 0;
+
+    private int blockType;
 
 
     public GamePanel() {
         setSize(400, 790);
+
+        try {
+            blocks.put(1, ImageIO.read(new File("data/bluebrick.png")));
+            blocks.put(2, ImageIO.read(new File("data/greenbrick.png")));
+            blocks.put(3, ImageIO.read(new File("data/orangebrick.png")));
+            blocks.put(4, ImageIO.read(new File("data/purplebrick.png")));
+            blocks.put(5, ImageIO.read(new File("data/redbrick.png")));
+            blocks.put(6, ImageIO.read(new File("data/tealbrick.png")));
+            blocks.put(7, ImageIO.read(new File("data/yellowbrick.png")));
+            blocks.put(100, ImageIO.read(new File("data/graybrick.png")));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         board = new Integer[19][10];
 
         for (int i = 0; i < 19; i ++) {
             for (int j = 0; j < 10; j++) {
                 if (j == 0 || j == 9){
-                    board[i][j] = 1;
+                    board[i][j] = 100;
                 } else {
                     board[i][j] = 0;
                 }
             }
         }
 
-        x = 4;
-        y = 0;
+        blockType = (int) (Math.random() * 7) + 1;
 
         pieceStates = states.get((int) (Math.random()*7));
         stateNum = (int) (Math.random()*pieceStates.length);
         piece = pieceStates[stateNum];
+
+        x = 4;
+        y = 0;
 
         addKeyListener(this);
         setFocusable(true);
@@ -161,7 +185,7 @@ class GamePanel extends JPanel implements KeyListener {
                 if (xx > 9 || yy > 19) {
                     return true;
 
-                } else if (piece[yy][xx] == 1 && board[yy+y][xx+x] == 1) {
+                } else if (piece[yy][xx] != 0 && board[yy+y][xx+x] != 0) {
                     return true;
                 }
             }
@@ -227,12 +251,14 @@ class GamePanel extends JPanel implements KeyListener {
         for (int yy = 0; yy < piece.length; yy++) {
             for (int xx = 0; xx < piece[0].length; xx++) {
                 if (piece[yy][xx] == 1) {
-                    board[y+yy][x+xx] = piece[yy][xx];
+                    board[y+yy][x+xx] = blockType;
                 }
             }
         }
         y = 0;
         x = 4;
+
+        blockType = (int) (Math.random() * 7) + 1;
 
         pieceStates = states.get((int) (Math.random()*7));
         stateNum = (int) (Math.random()*pieceStates.length);
@@ -241,7 +267,7 @@ class GamePanel extends JPanel implements KeyListener {
         placed = true;
 
         for (int xx = 1; xx < 9; xx++) {
-            if (board[0][xx] == 1) {
+            if (board[0][xx] != 0) {
                 System.out.println("U lose");
             }
         }
@@ -265,7 +291,7 @@ class GamePanel extends JPanel implements KeyListener {
                 score ++;
                 System.out.println(score);
                 currentboard.remove(i);
-                currentboard.add(0, new Integer[]{1,0,0,0,0,0,0,0,0,1});
+                currentboard.add(0, new Integer[]{100,0,0,0,0,0,0,0,0,100});
             }
         }
 
@@ -301,28 +327,19 @@ class GamePanel extends JPanel implements KeyListener {
 
         for (int yy = 0; yy < piece.length; yy++) {
             for (int xx = 0; xx < piece[0].length; xx++) {
-                if (piece[yy][xx] == 1) {
-                    g.setColor(Color.WHITE);
-                    g.fillRect((x+xx) * 40, (y+yy) * 40, 40, 40);
-                    g.setColor(Color.BLACK);
-                    g.fillRect((x+xx) * 40+10, (y+yy) * 40+10, 20, 20);
+                if (piece[yy][xx] != 0) {
+                    g.drawImage(blocks.get(blockType), (x+xx)*40, (y+yy)*40, 40, 40, null);
                 }
             }
         }
-
 
         for (int yy = 0; yy < 19; yy++) {
             for (int xx = 0; xx < 10; xx++) {
-                if (board[yy][xx].equals(1)) {
-                    g.setColor(Color.WHITE);
-                    g.fillRect(xx*40, yy*40, 40, 40);
-                    g.setColor(Color.BLACK);
-                    g.fillRect(xx * 40+10, yy * 40+10, 20, 20);
+                if (!board[yy][xx].equals(0)) {
+                    g.drawImage(blocks.get(board[yy][xx]), xx*40, yy*40, 40, 40, null);
                 }
             }
         }
-
-
-
+        g.dispose();
     }
 }
